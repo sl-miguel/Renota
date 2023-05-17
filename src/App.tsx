3cdeb221b9e05c2c7a1b7cf17bad3c9e01f40345
Miguel S <sl.miguel.acc@gmail.com>
2023-05-17 08:29:10 +0200

    🧹 Cleaning App

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,7 @@
-import { useContext } from 'react';
-import Search from './components/Search';
-import Note from './components/Note';
-import { NoteContext } from './context/NoteContext';
-import { NoteContextType } from './types/Note';
-import Logo from './components/Logo';
-import { randomizedNote } from './utils/randomized-note';
+import Home from './pages/Home';
 
 function App() {
-	const { notes, saveNote, setNotes } = useContext(NoteContext) as NoteContextType;
-
-	const handleColor = (id: string, color: string) => {
-		console.log('Handling Colors');
-		setNotes((previous) => previous.map((note) => (note.id === id ? { ...note, color } : note)));
-	};
-
-	const debugNote = () => {
-		const note = randomizedNote(notes);
-		saveNote(note);
-	};
-
-	return (
-		<div className=' mx-auto w-[97%]'>
-			<div className='my-10'>
-				<div className='flex flex-col items-center mb-6 cursor-pointer'>
-					<div onClick={debugNote} className='shake'>
-						<Logo size={170} />
-					</div>
-				</div>
-				<Search />
-			</div>
-			<div className='columns-1 sm:columns-2 lg:columns-3 2xl:columns-4 gap-4 py-4'>
-				{notes.map((note) => (
-					<Note key={note.id} note={note} handleColor={handleColor} />
-				))}
-			</div>
-		</div>
-	);
+	return <Home />;
 }
 
 export default App;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,39 @@
+import { useContext } from 'react';
+import Search from '../components/Search';
+import Note from '../components/Note';
+import { NoteContext } from '../context/NoteContext';
+import { NoteContextType } from '../types/Note';
+import Logo from '../components/Logo';
+import { randomizedNote } from '../utils/randomized-note';
+
 function Home() {
+	const { notes, saveNote, setNotes } = useContext(NoteContext) as NoteContextType;
+
+	const handleColor = (id: string, color: string) => {
+		console.log('Handling Colors');
+		setNotes((previous) => previous.map((note) => (note.id === id ? { ...note, color } : note)));
+	};
+
+	const debugNote = () => {
+		const note = randomizedNote(notes);
+		saveNote(note);
+	};
+
 	return (
-		<div>
-			<h1>Home</h1>
+		<div className=' mx-auto w-[97%]'>
+			<div className='my-10'>
+				<div className='flex flex-col items-center mb-6'>
+					<div onClick={debugNote} className='shake | cursor-pointer'>
+						<Logo size={170} />
+					</div>
+				</div>
+				<Search />
+			</div>
+			<div className='columns-1 sm:columns-2 lg:columns-3 2xl:columns-4 gap-4 py-4'>
+				{notes.map((note) => (
+					<Note key={note.id} note={note} handleColor={handleColor} />
+				))}
+			</div>
 		</div>
 	);
 }
